01ca1d89214320bb67a2b727fdf2e52cf62720bc
dmgoeller <denis@dmgoeller.de>
2024-06-16 08:51:36 +0200

    Rescue handler tests

--- test/jsapi/meta/rescue_handler_test.rb
+++ test/jsapi/meta/rescue_handler_test.rb
@@ -5,9 +5,24 @@ require 'test_helper'
 module Jsapi
   module Meta
     class RescueHandlerTest < Minitest::Test
-      def test_raises_an_exception_on_invalid_argument
+      def test_status_on_standard_error
+        rescue_handler = RescueHandler.new(StandardError, status: 500)
+        assert_equal(500, rescue_handler.status)
+      end
+
+      def test_status_on_subclass_of_standard_error
+        rescue_handler = RescueHandler.new(RuntimeError, status: 500)
+        assert_equal(500, rescue_handler.status)
+      end
+
+      def test_raises_an_exception_on_other_object_than_a_class
         error = assert_raises(ArgumentError) { RescueHandler.new('foo') }
         assert_equal('"foo" must be a standard error class', error.message)
+      end
+
+      def test_raises_an_exception_on_other_error_class
+        error = assert_raises(ArgumentError) { RescueHandler.new(Exception) }
+        assert_equal('Exception must be a standard error class', error.message)
       end
 
       def test_inspect
